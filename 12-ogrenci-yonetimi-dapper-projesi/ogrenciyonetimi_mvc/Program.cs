9067af26d5721f399fc04a289a432ed348b37ad9
Merve Gezginci <ymgezginci9@gmail.com>
2026-07-07 15:12:56 +0300

    Fix Dashboard routing issues by setting Home as default route and pointing nav links explicitly to Home/Index

--- 12-ogrenci-yonetimi-dapper-projesi/ogrenciyonetimi_mvc/Program.cs
+++ 12-ogrenci-yonetimi-dapper-projesi/ogrenciyonetimi_mvc/Program.cs
@@ -43,6 +43,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Student}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
